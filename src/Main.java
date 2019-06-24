008bc1340585fdbd7ea7b0c1526cb6db09b01a43
jphomick <jphomick@gmail.com>
2019-06-24 16:40:40 -0400

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
@@ -12,6 +13,30 @@ public class Main {
         }
 
         output("Done.");
+
+        Scanner keyboard = new Scanner(System.in);
+
+        String userInput = "dummy";
+        System.out.println("Enter as much text as you want. Type 'q' to quit.");
+
+        while (!userInput.equals("q")) {
+            userInput = keyboard.next();
+            System.out.println(userInput);
+        }
+
+        int num, total, nums;
+        nums = 0;
+        total = 0;
+
+        System.out.println("Enter 10 numbers");
+
+        while (nums < 10) {
+            num = keyboard.nextInt();
+            total += num;
+            nums++;
+        }
+
+        System.out.println("The total of all 10 numbers is " + total);
     }
 
     private static void output(String aMessage) {
